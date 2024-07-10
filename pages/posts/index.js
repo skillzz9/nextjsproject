767979ee68ba49aef53c374d644057b2b7e45453
skillzz9 <hpos7501@uni.sydney.edu.au>
2024-07-10 12:05:00 +1000

    implemented server side rendering with a fake API

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,6 +1,42 @@
-export default function Posts(){
+import { useEffect, useState } from "react"
+
+export async function getServerSideProps(){
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+
+    return {
+    props:{
+        posts: data
+    }
+    }
+}
+
+export default function Posts({posts}){
+
+    // const [posts, setPosts] = useState([]);
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    //         const data = await res.json();
+    //         setPosts(data);
+
+
+    //     }
+
+    //     fetchData();
+    // }, [])
+
+
     return(
-<h1>This is the posts page</h1>
+    <>
+        <h1>This is the posts page</h1>
+        {posts?.map(post => {
+            return <li key={post.id}>{post.title}</li>
+        })}
+    </>
+
+
     )
 
 }
